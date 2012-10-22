b314d9d1e158ef0cad445e55e9f3a9dae77fd812
Arne Claassen <arne@claassen.net>
2012-10-21 19:13:19 -0700

    implemented kill buffer with kill and yank support

--- js/readline.js
+++ js/readline.js
@@ -19,9 +19,13 @@
     46:'DELETE'
   };
 
-  ReadLine = function (window, document) {
+  ReadLine = function (config) {
+    config = config || {};
 
     // instance fields
+    var _window = config.window || window;
+    var _document = config.document || document;
+    var _history = config.history || new ReadLine.History();
     var _active = false;
     var _onCompletion;
     var _onEnter;
@@ -30,153 +34,8 @@
     var _text = '';
     var _cursor = 0;
     var _strUtil = ReadLine.StrUtil;
-
-    // set up key capture
-    document.onkeydown = function (e) {
-      e = e || window.event
-      if (!_active) {
-        return true;
-      }
-      console.log("keydown - code: " + e.keyCode);
-      var handled = true;
-      switch (e.keyCode) {
-        case 8:  // Backspace
-          cmdBackpace();
-          break;
-        case 9:  // Tab
-          cmdComplete();
-          break;
-        case 13: // Enter
-          cmdDone();
-          break;
-        case 35: // End
-          cmdEnd();
-          break;
-        case 36: // Home
-          cmdHome();
-          break;
-        case 37: // Left
-          cmdLeft();
-          break;
-        case 38: // Up
-          cmdHistoryPrev();
-          break;
-        case 39: // Right
-          cmdRight();
-          break;
-        case 40: // Down
-          cmdHistoryNext();
-          break;
-        case 46: // Delete
-          cmdDeleteChar();
-          break;
-
-        // these we catch and have no commands for
-        case 10: // Pause
-        case 19: // Caps Lock
-        case 27: // Esc
-        case 33: // Page Up
-        case 34: // Page Down
-        case 45: // Insert
-
-        // all others we don't handle at this level
-        default:
-          handled = false;
-          break;
-      }
-      if (!handled) {
-
-        // intercept ctrl- and alt- sequences
-        if (e.ctrlKey && !e.shiftKey && !e.altKey) {
-          switch (e.keyCode) {
-            case 65: // A
-              cmdHome();
-              handled = true;
-              break;
-            case 69: // E
-              cmdEnd();
-              handled = true;
-              break;
-            case 66: // B
-              cmdLeft();
-              handled = true;
-              break;
-            case 70: // F
-              cmdRight();
-              handled = true;
-              break;
-            case 80: // P
-              cmdHistoryPrev();
-              handled = true;
-              break;
-            case 78: // N
-              cmdHistoryNext();
-              handled = true;
-              break;
-            case 75: // K
-              cmdKillToEOF();
-              handled = true;
-              break;
-            case 89: // Y
-              cmdYank();
-              handled = true;
-              break;
-            case 68: // D
-              cmdDeleteChar();
-              handled = true;
-              break;
-            case 76: // L
-              cmdRefresh();
-              handled = true;
-              break;
-            case 82: // R
-              cmdReverseSearch();
-              handled = true;
-              break;
-          }
-        } else if (e.altKey && !e.ctrlKey && !e.shiftKey) {
-          switch (e.keyCode) {
-            case 66: // B
-              cmdBackwardWord();
-              handled = true;
-              break;
-            case 70: // F
-              cmdForwardWord();
-              handled = true;
-              break;
-          }
-        }
-      }
-      if (!handled) {
-        return true;
-      }
-      var info = getKeyInfo(e);
-      if (_onKeydown) {
-        _onKeydown({
-          code:e.keyCode,
-          shift:e.shiftKey,
-          control:e.controlKey,
-          alt:e.altKey,
-          name:SPECIAL[e.keyCode],
-          isChar:false
-        });
-      }
-      e.preventDefault();
-      return false;
-    };
-    document.onkeypress = function (e) {
-      if (!_active) {
-        return true;
-      }
-      console.log("keypress - code: " + e.keyCode + ", char: " + String.fromCharCode(e.keyCode) + ", ctrl: " + e.ctrlKey);
-      var key = getKeyInfo(e);
-      addText(key.char);
-      if (_onKeydown) {
-        _onKeydown(key);
-      }
-      e.preventDefault();
-      return false;
-    };
+    var _kill_buffer = '';
+    var _lastCmd;
 
     // public methods
     var self = {
@@ -191,6 +50,15 @@
       },
       onChange:function (callback) {
         _onChange = callback;
+      },
+      onEnter:function(callback) {
+        _onEnter = callback;
+      },
+      getLine:function() {
+        return {
+          text:_text,
+          cursor:_cursor
+        };
       }
     };
 
@@ -210,13 +78,19 @@
       return info;
     }
 
+    function call(cmd) {
+      console.log('calling: '+cmd.name+', previous: '+_lastCmd);
+      _lastCmd = cmd.name;
+      cmd();
+    }
+
     function cmdBackpace() {
       if (_cursor == 0) {
         return;
       }
       --_cursor;
       _text = _strUtil.remove(_text, _cursor, _cursor + 1);
-      updateCursor(_cursor);
+      refresh();
     }
 
     function cmdComplete() {
@@ -224,15 +98,18 @@
     }
 
     function cmdDone() {
-      if (_onEnter) {
-        _onEnter(_text);
+      if(!_text) {
+        return;
       }
+      var text = _text;
       _text = '';
       _cursor = 0;
+      if (_onEnter) {
+        _onEnter(text,self.getLine());
+      }
     }
 
     function cmdEnd() {
-
       updateCursor(_text.length);
     }
 
@@ -247,10 +124,6 @@
       updateCursor(_cursor - 1);
     }
 
-    function cmdHistoryPrev() {
-
-    }
-
     function cmdRight() {
       if (_cursor == _text.length) {
         return;
@@ -258,23 +131,35 @@
       updateCursor(_cursor + 1);
     }
 
+    function cmdHistoryPrev() {
+
+    }
+
     function cmdHistoryNext() {
 
     }
 
     function cmdDeleteChar() {
-
+      if (_cursor == _text.length) {
+        return;
+      }
+      _text = _strUtil.remove(_text, _cursor, _cursor + 1);
+      refresh();
     }
 
     function cmdKillToEOF() {
+      _kill_buffer = _text.substr(_cursor);
+      _text = _text.substr(0,_cursor);
+      refresh();
     }
 
     function cmdYank() {
-
+      _text = _strUtil.insert(_text,_cursor,_kill_buffer);
+      updateCursor(_cursor+_kill_buffer.length);
     }
 
     function cmdRefresh() {
-
+      refresh();
     }
 
     function cmdReverseSearch() {
@@ -302,12 +187,157 @@
 
     function refresh() {
       if (_onChange) {
-        _onChange({
-          text:_text,
-          cursor:_cursor
-        });
+        _onChange(self.getLine());
       }
     }
+
+    // set up key capture
+    document.onkeydown = function (e) {
+      e = e || window.event
+      if (!_active) {
+        return true;
+      }
+      //console.log("keydown - code: " + e.keyCode);
+      var handled = true;
+      switch (e.keyCode) {
+        case 8:  // Backspace
+          call(cmdBackpace);
+          break;
+        case 9:  // Tab
+          call(cmdComplete);
+          break;
+        case 13: // Enter
+          call(cmdDone);
+          break;
+        case 35: // End
+          call(cmdEnd);
+          break;
+        case 36: // Home
+          call(cmdHome);
+          break;
+        case 37: // Left
+          call(cmdLeft);
+          break;
+        case 38: // Up
+          call(cmdHistoryPrev);
+          break;
+        case 39: // Right
+          call(cmdRight);
+          break;
+        case 40: // Down
+          call(cmdHistoryNext);
+          break;
+        case 46: // Delete
+          call(cmdDeleteChar);
+          break;
+
+        // these we catch and have no commands for
+        case 10: // Pause
+        case 19: // Caps Lock
+        case 27: // Esc
+        case 33: // Page Up
+        case 34: // Page Down
+        case 45: // Insert
+
+        // all others we don't handle at this level
+        default:
+          handled = false;
+          break;
+      }
+      if (!handled) {
+
+        // intercept ctrl- and alt- sequences
+        if (e.ctrlKey && !e.shiftKey && !e.altKey) {
+          switch (e.keyCode) {
+            case 65: // A
+              call(cmdHome);
+              handled = true;
+              break;
+            case 69: // E
+              call(cmdEnd);
+              handled = true;
+              break;
+            case 66: // B
+              call(cmdLeft);
+              handled = true;
+              break;
+            case 70: // F
+              call(cmdRight);
+              handled = true;
+              break;
+            case 80: // P
+              call(cmdHistoryPrev);
+              handled = true;
+              break;
+            case 78: // N
+              call(cmdHistoryNext);
+              handled = true;
+              break;
+            case 75: // K
+              call(cmdKillToEOF);
+              handled = true;
+              break;
+            case 89: // Y
+              call(cmdYank);
+              handled = true;
+              break;
+            case 68: // D
+              call(cmdDeleteChar);
+              handled = true;
+              break;
+            case 76: // L
+              call(cmdRefresh);
+              handled = true;
+              break;
+            case 82: // R
+              call(cmdReverseSearch);
+              handled = true;
+              break;
+          }
+        } else if (e.altKey && !e.ctrlKey && !e.shiftKey) {
+          switch (e.keyCode) {
+            case 66: // B
+              call(cmdBackwardWord);
+              handled = true;
+              break;
+            case 70: // F
+              call(cmdForwardWord);
+              handled = true;
+              break;
+          }
+        }
+      }
+      if (!handled) {
+        return true;
+      }
+      var info = getKeyInfo(e);
+      if (_onKeydown) {
+        _onKeydown({
+          code:e.keyCode,
+          shift:e.shiftKey,
+          control:e.controlKey,
+          alt:e.altKey,
+          name:SPECIAL[e.keyCode],
+          isChar:false
+        });
+      }
+      e.preventDefault();
+      return false;
+    };
+    document.onkeypress = function (e) {
+      if (!_active) {
+        return true;
+      }
+      //console.log("keypress - code: " + e.keyCode + ", char: " + String.fromCharCode(e.keyCode) + ", ctrl: " + e.ctrlKey);
+      _lastCmd = null;
+      var key = getKeyInfo(e);
+      addText(key.char);
+      if (_onKeydown) {
+        _onKeydown(key);
+      }
+      e.preventDefault();
+      return false;
+    };
 
     return self
   };
@@ -316,11 +346,6 @@
     remove:function (text, from, to) {
       if (text.length <= 1 || text.length <= to - from) {
         return '';
-      }
-      if (to == text.length - 1) {
-
-        // delete trailing characters
-        return text.substr(0, text.length - from);
       }
       if (from == 0) {
 
@@ -344,6 +369,12 @@
     }
   };
 
+  ReadLine.History = function() {
+
+    return {
+
+    };
+  };
 })();
 
 
--- js/shell.js
+++ js/shell.js
@@ -1,13 +1,16 @@
-(function ($,document,window) {
+(function ($, document, window) {
   Shell = function (config) {
-
+    config = config || {};
     // instance fields
     var _prompt = config.prompt || 'jsh$';
+    var _shell_id = config.shell_id || '#shell';
     var _input_id = config.input_id || '#shell-cli';
+    var _input_html = config.input_html || '<div id="shell-cli"><span class="prompt"></span>&nbsp;<span class="input"><span class="left"/><span class="cursor"/><span class="right"/></span></div>';
     var _blinktime = config.blinktime || 500;
-    var _readline = config.readline;
+    var _readline = config.readline || new ReadLine();
     var _active = false;
     var _cursor_visible = false;
+    var _onCmd;
     var _line = {
       text:'',
       cursor:0
@@ -16,6 +19,7 @@
     // public methods
     var self = {
       activate:function () {
+        init();
         self.refresh();
         _active = true;
         _readline.activate();
@@ -27,24 +31,28 @@
       },
       setPrompt:function (prompt) {
         _prompt = prompt;
-        if(!_active) {
+        if (!_active) {
           return;
         }
         self.refresh();
       },
       setInput:function (text, cursor) {
         _line = {text:text, cursor:cursor};
-        if(!_active) {
+        if (!_active) {
           return;
         }
         self.refresh();
+      },
+      onCmd:function(callback) {
+        _onCmd = callback;
       },
       render:function () {
         var left = _line.text.substr(0, _line.cursor);
         var cursor = _line.text.substr(_line.cursor, 1);
         var right = _line.text.substr(_line.cursor + 1);
+        $(_input_id + ' .prompt').text(_prompt);
         $(_input_id + ' .input .left').text(left);
-        if(!cursor) {
+        if (!cursor) {
           $(_input_id + ' .input .cursor').html('&nbsp;').css('textDecoration', 'underline');
         } else {
           $(_input_id + ' .input .cursor').text(cursor).css('textDecoration', 'underline');
@@ -54,29 +62,38 @@
         console.log('rendered "' + _line.text + '" w/ cursor at ' + _line.cursor);
       },
       refresh:function () {
-        $(_input_id+' .prompt').text(_prompt);
-        $(_input_id+' .input').html('<span class="left"/><span class="cursor"/><span class="right"/>');
+        $(_input_id).replaceWith(_input_html);
         self.render();
-        console.log('refreshed '+_input_id);
+        console.log('refreshed ' + _input_id);
       }
     };
 
     function blinkCursor() {
-      if(!_active) {
+      if (!_active) {
         return;
       }
-      window.setTimeout(function(){
-        if(!_active) {
+      window.setTimeout(function () {
+        if (!_active) {
           return;
         }
         _cursor_visible = !_cursor_visible;
-        if(_cursor_visible) {
+        if (_cursor_visible) {
           $(_input_id + ' .input .cursor').css('textDecoration', 'underline');
         } else {
           $(_input_id + ' .input .cursor').css('textDecoration', '');
         }
         blinkCursor();
-      },_blinktime);
+      }, _blinktime);
+    }
+
+    function init() {
+      if ($(_shell_id).length == 0) {
+        _active = false;
+        return;
+      }
+      if ($(_input_id).length == 0) {
+        $(_shell_id).append(_input_html);
+      }
     }
 
     // init
@@ -84,6 +101,16 @@
       _line = line;
       self.render();
     });
+    _readline.onEnter(function(cmd,line) {
+      console.log("got command: "+ cmd);
+      if(_onCmd) {
+        _onCmd(cmd,_input_id);
+      }
+      $(_input_id).removeAttr('id');
+      $(_shell_id).append(_input_html);
+      _line = line;
+      self.refresh();
+    });
     return self;
   };
-})(jQuery,document,window);
+})(jQuery, document, window);
