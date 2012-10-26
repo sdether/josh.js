8be0f86b1f1aa7405badc858db87fb5773115e52
Arne Claassen <arne@claassen.net>
2012-10-25 17:46:18 -0700

    wip

--- js/mtshell.js
+++ js/mtshell.js
@@ -1,28 +1,35 @@
-(function($, document, window) {
-  MtShell = function(config) {
+(function ($, _, document, window, localStorage) {
+  MtShell = function (config) {
     var _shell = new Shell();
-    var _commandList = ['go', 'ls', 'cd', 'show'];
+    var _commandList = ['go', 'ls', 'cd', 'show', 'pwd'];
     var _namespaces = {
-      0: "",
-      2: "User:",
-      10: "Template:",
-      101: "Special:"
+      0:"",
+      2:"User:",
+      10:"Template:",
+      101:"Special:"
     };
     var _home = null;
     var _current = null;
-
+    var _localState = {
+      active:false
+    };
     // public methods
     var self = {
-      activate: function() {
-        if(!_home) {
-          getPage("/", function(page) {
+      activate:function () {
+        if (!_home) {
+          getPage("/", function (page) {
             _home = page;
-            setCurrent(_home);
-            _shell.activate();
+            getPage(window.location.pathname, function (page) {
+              var prompt = setCurrent(page);
+              _shell.setPrompt(prompt);
+              _shell.activate();
+            });
           });
+        } else {
+          _shell.activate();
         }
       },
-      deactivate: function() {
+      deactivate:function () {
         _shell.deactivate();
       }
     };
@@ -32,12 +39,12 @@
       var partial = line.text.substr(0, line.cursor);
       var parts = split(partial);
       console.log(parts);
-      if(parts.length == 0) {
-        return callback({suggestions: _commandList});
+      if (parts.length == 0) {
+        return callback({suggestions:_commandList});
       }
-      if(parts.length == 1) {
+      if (parts.length == 1) {
         var cmd = parts[0];
-        if(startsWith(cmd, ".") || startsWith(cmd, "/")) {
+        if (startsWith(cmd, ".") || startsWith(cmd, "/")) {
           return completePath(cmd, callback);
         }
         return callback(bestMatch(cmd, _commandList));
@@ -46,21 +53,21 @@
     }
 
     function completePath(path, callback) {
-      $.getJSON("http://ariel.mindtouch.com/@api/deki/console/matches?path=" + path, function(data) {
+      $.getJSON("/@api/deki/console/matches?path=" + path, function (data) {
         console.log(data);
         callback(bestMatch(path, data.Matches));
       });
     }
 
     function getPage(path, callback) {
-      if(_current && (!path || path == '.')) {
+      if (_current && (!path || path == '.')) {
         return callback(_current);
       }
-      if(_home && path == '/') {
+      if (_home && path == '/') {
         return callback(_home);
       }
-      $.getJSON("http://ariel.mindtouch.com/@api/deki/console/node?path=" + path, function(data) {
-        console.log("page:"+data);
+      $.getJSON("/@api/deki/console/node?path=" + path, function (data) {
+        console.log("page:" + data);
         callback(data);
       });
     }
@@ -69,31 +76,39 @@
       var parts = split(cmdtext);
       var cmd = parts[0];
       var path = parts[1];
-      if(startsWith(cmd, ".") || startsWith(cmd, "/")) {
+      if (startsWith(cmd, ".") || startsWith(cmd, "/")) {
         path = cmd;
         cmd = "show";
       }
-      switch(cmd) {
+      switch (cmd) {
         case "go":
-          callback();
+          getPage(path, function (page) {
+            var path = getPath(page);
+            console.log("navigating to " + path);
+            window.location = path;
+          });
           return;
         case"ls":
           callback();
           return;
+        case"pwd":
+          $(input_id).after(getPath(_current));
+          callback();
+          return;
         case "cd":
-          getPage(path, function(page) {
-            setCurrent(page);
-            callback();
+          getPage(path, function (page) {
+            callback(setCurrent(page));
           });
           return;
         case "show":
-          getPage(path, function(page) {
+          getPage(path, function (page) {
             show(input_id, page, callback);
           });
           return;
         default:
-          var content = $('<div><strong>Unrecognized command:&nbsp;</strong><span class="cmd"></span></div>');
-          $(content).find('.cmd').text(cmd);
+          //var content = $('<div><strong>Unrecognized command:&nbsp;</strong><span class="cmd"></span></div>');
+          //$(content).find('.cmd').text(cmd);
+          var content = _.template('<div><strong>Unrecognized command:&nbsp;</strong><%=cmd%></div>', {cmd:cmd});
           $(input_id).after(content);
           callback();
           return;
@@ -102,34 +117,35 @@
 
     function setCurrent(page) {
       _current = page;
-      setPrompt();
+      return getPath(_current) + " $";
     }
 
-    function setPrompt() {
-      _shell.setPrompt(" /" + _namespaces[_current.Namespace] + _current.Path + " ["+_current.Id+ "] $ ");
+    function getPath(page) {
+      return " /" + _namespaces[page.Namespace] + page.Path;
     }
 
     function show(input_id, page, callback) {
       var content = $(
-        '<div>'+
-          '<div><strong>Id:&nbsp;</strong><span class="id"></span></div>'+
-          '<div><strong>Path:&nbsp;</strong><span class="path"></span></div>'+
-          '<div><strong>Displayname:&nbsp;</strong><span class="display"></span></div>'+
-          '<div><strong>Parent Id:&nbsp;</strong><span class="parentid"></span></div>'+
-          '<div><strong>Child Ids:&nbsp;</strong><span class="childids"></span></div>'+
-        '</div>'
+        '<div>' +
+          '<div><strong>Id:&nbsp;</strong><span class="id"></span></div>' +
+          '<div><strong>Path:&nbsp;</strong><span class="path"></span></div>' +
+          '<div><strong>Displayname:&nbsp;</strong><span class="display"></span></div>' +
+          '<div><strong>Parent Id:&nbsp;</strong><span class="parentid"></span></div>' +
+          '<div><strong>Child Ids:&nbsp;</strong><span class="childids"></span></div>' +
+          '</div>'
       );
       $(content).find('.id').text(page.Id);
       $(content).find('.path').text(" /" + _namespaces[page.Namespace] + page.Path);
       $(content).find('.display').text(page.DisplayName);
       $(content).find('.parentid').text(page.ParentId);
-      $(content).find('.childids').text("["+page.ChildIds+"]");
+      $(content).find('.childids').text("[" + page.ChildIds + "]");
       $(input_id).after(content);
       callback();
     }
+
     function split(str) {
       var parts = str.split(/\s+/);
-      if(parts.length > 0 && !parts[parts.length - 1]) {
+      if (parts.length > 0 && !parts[parts.length - 1]) {
         parts.pop();
       }
       return parts;
@@ -142,18 +158,18 @@
     function bestMatch(partial, possible) {
       var completions = [];
       var common = '';
-      for(var i = 0; i < possible.length; i++) {
+      for (var i = 0; i < possible.length; i++) {
         var option = possible[i];
-        if(option.slice(0, partial.length) == partial) {
+        if (option.slice(0, partial.length) == partial) {
           completions.push(option);
-          if(!common) {
+          if (!common) {
             common = option;
             console.log("initial common:" + common);
-          } else if(option.slice(0, common.length) != common) {
+          } else if (option.slice(0, common.length) != common) {
             console.log("find common stem for '" + common + "' and '" + option + "'");
             var j = partial.length;
-            while(j < common.length && j < option.length) {
-              if(common[j] != option[j]) {
+            while (j < common.length && j < option.length) {
+              if (common[j] != option[j]) {
                 common = common.substr(0, j);
                 break;
               }
@@ -162,15 +178,15 @@
           }
         }
       }
-      if(completions.length == 0) {
+      if (completions.length == 0) {
         return;
       }
-      if(completions.length == 1) {
+      if (completions.length == 1) {
         completions = null;
       }
       return {
-        result: common.substr(partial.length),
-        suggestions: completions
+        result:common.substr(partial.length),
+        suggestions:completions
       };
     }
 
@@ -180,4 +196,4 @@
 
     return self;
   };
-})(jQuery, document, window);
+})(jQuery, _, document, window, localStorage);
--- js/shell.js
+++ js/shell.js
@@ -1,5 +1,5 @@
-(function($, document, window) {
-  Shell = function(config) {
+(function ($, document, window) {
+  Shell = function (config) {
     config = config || {};
     // instance fields
     var _prompt = config.prompt || 'jsh$';
@@ -10,106 +10,114 @@
     var _suggest_id = config.suggest_id = "#shell-suggest";
     var _blinktime = config.blinktime || 500;
     var _history = config.history || new ReadLine.History();
-    var _readline = config.readline || new ReadLine({history: history});
+    var _readline = config.readline || new ReadLine({history:history});
     var _active = false;
     var _cursor_visible = false;
     var _onCmd;
     var _suggestion;
     var _line = {
-      text: '',
-      cursor: 0
+      text:'',
+      cursor:0
     };
+    var shell;
 
     // public methods
     var self = {
-      activate: function() {
+      activate:function () {
         init();
         self.refresh();
         _active = true;
         _readline.activate();
         blinkCursor();
       },
-      deactivate: function() {
+      deactivate:function () {
         _active = false;
         _readline.deactivate();
       },
-      setPrompt: function(prompt) {
+      setPrompt:function (prompt) {
         _prompt = prompt;
-        if(!_active) {
+        if (!_active) {
           return;
         }
         self.refresh();
       },
-      setInput: function(text, cursor) {
-        _line = {text: text, cursor: cursor};
-        if(!_active) {
+      setInput:function (text, cursor) {
+        _line = {text:text, cursor:cursor};
+        if (!_active) {
           return;
         }
         self.refresh();
       },
-      onCmd: function(cmdHandler) {
+      onCmd:function (cmdHandler) {
         _onCmd = cmdHandler;
       },
-      onCompletion: function(completionHandler) {
-        _readline.onCompletion(function(line, callback) {
-          if(_suggestion) {
+      onCompletion:function (completionHandler) {
+        _readline.onCompletion(function (line, callback) {
+          if (_suggestion) {
             $(_suggest_id).remove();
             _suggestion = null;
           }
-          if(!line) {
+          if (!line) {
             return;
           }
-          completionHandler(line, function(completion) {
-            console.log("completion: "+completion)
-            if(!completion) {
+          completionHandler(line, function (completion) {
+            console.log("completion: " + completion)
+            if (!completion) {
               callback();
               return;
             }
-            if(completion.suggestions) {
+            if (completion.suggestions) {
               _suggestion = $(_suggest_html);
-              for(var i = 0; i < completion.suggestions.length; i++) {
+              for (var i = 0; i < completion.suggestions.length; i++) {
                 console.log("suggestion: " + completion.suggestions[i]);
                 _suggestion.append($("<div></div>").text(completion.suggestions[i]));
               }
               console.log(_suggestion);
               $(_input_id).after(_suggestion);
             }
+            self.scrollToBottom();
             callback(completion.result);
           });
         });
       },
-      render: function() {
+      render:function () {
         var left = _line.text.substr(0, _line.cursor);
         var cursor = _line.text.substr(_line.cursor, 1);
         var right = _line.text.substr(_line.cursor + 1);
         $(_input_id + ' .prompt').text(_prompt);
         $(_input_id + ' .input .left').text(left);
-        if(!cursor) {
+        if (!cursor) {
           $(_input_id + ' .input .cursor').html('&nbsp;').css('textDecoration', 'underline');
         } else {
           $(_input_id + ' .input .cursor').text(cursor).css('textDecoration', 'underline');
         }
         $(_input_id + ' .input .right').text(right);
         _cursor_visible = true;
+        self.scrollToBottom();
         console.log('rendered "' + _line.text + '" w/ cursor at ' + _line.cursor);
       },
-      refresh: function() {
+      refresh:function () {
         $(_input_id).replaceWith(_input_html);
         self.render();
         console.log('refreshed ' + _input_id);
+
+      },
+      scrollToBottom:function () {
+        //_shell.scrollTop(_shell.height());
+        _shell.animate({scrollTop:_shell.height()}, 1000);
       }
     };
 
     function blinkCursor() {
-      if(!_active) {
+      if (!_active) {
         return;
       }
-      window.setTimeout(function() {
-        if(!_active) {
+      window.setTimeout(function () {
+        if (!_active) {
           return;
         }
         _cursor_visible = !_cursor_visible;
-        if(_cursor_visible) {
+        if (_cursor_visible) {
           $(_input_id + ' .input .cursor').css('textDecoration', 'underline');
         } else {
           $(_input_id + ' .input .cursor').css('textDecoration', '');
@@ -119,26 +127,31 @@
     }
 
     function init() {
-      if($(_shell_id).length == 0) {
+      if ($(_shell_id).length == 0) {
         _active = false;
         return;
       }
-      if($(_input_id).length == 0) {
+      if ($(_input_id).length == 0) {
         $(_shell_id).append(_input_html);
       }
+      _shell = $(_shell_id);
+      _shell.css('overflow', 'scroll');
     }
 
     // init
-    _readline.onChange(function(line) {
+    _readline.onChange(function (line) {
       _line = line;
       self.render();
     });
-    _readline.onEnter(function(cmd, line, callback) {
+    _readline.onEnter(function (cmd, line, callback) {
       console.log("got command: " + cmd);
-      if(_onCmd) {
-        _onCmd(cmd, _input_id, function() {
+      if (_onCmd) {
+        _onCmd(cmd, _input_id, function (prompt) {
           $(_input_id).removeAttr('id');
           $(_shell_id).append(_input_html);
+          if (prompt) {
+            self.setPrompt(prompt);
+          }
           _line = line;
           self.refresh();
           callback();
