543679eb2df4a4d6df8d10a80fd3da42378e3041
Arne Claassen <arne@claassen.net>
2013-01-13 15:09:26 -0800

    Squashed commit of the following:
    commit eef51956957216d6945e7791057573cde35f0277
    Author: Arne Claassen <arne@claassen.net>
    Date:   Sun Jan 13 14:41:56 2013 -0800
    
        removed _exec and made _default do it all
    
    commit 56c88d504b07d7769db6d2368d6603cc775d42a4
    Author: Arne Claassen <arne@claassen.net>
    Date:   Sun Jan 13 14:15:26 2013 -0800
    
        re-vamping how default/exec handling is done

--- js/pathhandler.js
+++ js/pathhandler.js
@@ -22,26 +22,11 @@ var Josh = Josh || {};
       }
     });
     var _shell = shell;
+    var _original_default = _shell.getCommandHandler('_default');
     var self = {
-      handlers: {
-        ls: {
-          exec: ls,
-          completion: pathCompletionHandler
-        },
-        exec: {
-          exec: exec,
-          completion: commandAndPathCompletionHandler
-        },
-        cd: {
-          exec: cd,
-          completion: pathCompletionHandler
-        },
-        pwd: {
-          exec: pwd,
-          completion: pathCompletionHandler
-        }
-      },
+      current: null,
       pathCompletionHandler: pathCompletionHandler,
+      commandAndPathCompletionHandler: commandAndPathCompletionHandler,
       templates: {
         not_found: _.template("<div><%=cmd%>: <%=path%>: No such file or directory</div>"),
         ls: _.template("<div><% _.each(nodes, function(node) { %><span><%=node.name%>&nbsp;</span><% }); %></div>"),
@@ -56,24 +41,38 @@ var Josh = Josh || {};
       },
       getPrompt: function() {
         return self.templates.prompt({node: self.current});
-      },
-      current: null
+      }
     };
 
-    self.handlers._original_exec = _shell.getCommandHandler('_exec');
-    _shell.setCommandHandler("ls", self.handlers.ls);
-    _shell.setCommandHandler("pwd", self.handlers.pwd);
-    _shell.setCommandHandler("cd", self.handlers.cd);
-    _shell.setCommandHandler("_exec", self.handlers.exec);
+    _shell.setCommandHandler("ls", {
+      exec: ls,
+      completion: pathCompletionHandler
+    });
+    _shell.setCommandHandler("pwd", {
+      exec: pwd,
+      completion: pathCompletionHandler
+    });
+    _shell.setCommandHandler("cd", {
+      exec: cd,
+      completion: pathCompletionHandler
+    });
+    _shell.setCommandHandler("_default", {
+      exec: _original_default.exec,
+      completion: commandAndPathCompletionHandler
+    });
     _shell.onNewPrompt(function(callback) {
       callback(self.getPrompt());
     });
 
     function commandAndPathCompletionHandler(cmd, arg, line, callback) {
+      _console.log("calling command and path completion handler w/ cmd: '"+cmd+"', arg: '"+arg+"'");
+      if(!arg) {
+        arg = cmd;
+      }
       if(arg[0] == '.' || arg[0] == '/') {
         return pathCompletionHandler(cmd, arg, line, callback);
       }
-      return self.handlers._original_exec.completion(cmd, arg, line, callback);
+      return _original_default.completion(cmd, arg, line, callback);
     }
 
     function pathCompletionHandler(cmd, arg, line, callback) {
@@ -112,10 +111,6 @@ var Josh = Josh || {};
           return x.name;
         })));
       });
-    }
-
-    function exec(cmd, args, callback) {
-      callback();
     }
 
     function cd(cmd, args, callback) {
