6ade77e6194200a7093eb8b8db4ab4551ca27274
Arne Claassen <arne@claassen.net>
2013-03-05 07:16:07 -0800

    changing github auth api to always proxy requests to github

--- src/app.js
+++ src/app.js
@@ -8,20 +8,21 @@ var authLog = fs.createWriteStream(config.authLog, {flags: 'a'});
 app.configure(function() {
   app.use(express.cookieParser());
   app.use(express.bodyParser());
+  app.use(express.session(config.session));
+  app.use(accessControlHeaders);
 });
 
 
-app.get("/github", function(req, res) {
+app.get("/auth", function(req, res) {
   res.redirect("https://github.com/login/oauth/authorize?client_id=" + config.github.client_id);
 });
-app.get("/github-token", function(req, res) {
-  var token = req.cookies.access_token;
-  console.log("token: " + token);
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Origin', 'http://sdether.github.com');
-  res.send({access_token: token});
+app.get("/status", function(req,res) {
+  if(req.session.access_token) {
+    res.send({authenticated: true});
+  } else {
+    res.send({authenticated: false});
+  }
 });
-
 app.get("/github-auth", function(req, res) {
   console.log("code: " + req.query.code);
   request({
@@ -49,15 +50,39 @@ app.get("/github-auth", function(req, res) {
       function(error, response, body) {
         var user = JSON.parse(body);
         authLog.write(new Date().toJSON()+"\t"+user.login+"\n");
-        res.cookie('access_token', auth.access_token);
+        req.session.access_token = auth.access_token;
         res.redirect("http://sdether.github.com/josh.js/githubconsole.html");
       });
   });
 });
 app.get('/hello',function(req,res) {
 	res.send({hello:'world'});
+});
+app.get(/^\/(user|users|repos)(\/.*|$)/, function(req,res) {
+  var headers = {
+    Accept: 'application/json'
+  };
+  if(req.session.access_token) {
+    headers.Authorization = 'token ' + req.session.access_token;
+  }
+  request({
+      method: 'get',
+      uri: 'https://api.github.com'+req.path,
+      headers: headers
+    },
+    function(error, response, body) {
+      res.set('Content-Type','application/json; charset=utf-8');
+      res.send(body);
     });
+});
 app.get('*', function(req,res) {
   res.redirect("http://sdether.github.com/josh.js/");
-    });
+});
+
+function accessControlHeaders(req,res,next) {
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.set('Access-Control-Allow-Origin', 'http://sdether.github.com');
+  next();
+}
+
 app.listen(config.port);
