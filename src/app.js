0e976fd61b027148e60903a880114d7c05764db9
Arne Claassen <arne@claassen.net>
2013-03-07 23:17:34 -0800

    now proxing github api requests

--- src/app.js
+++ src/app.js
@@ -13,17 +13,17 @@ app.configure(function() {
 });
 
 
-app.get("/auth", function(req, res) {
+app.get("/authenticate", function(req, res) {
   res.redirect("https://github.com/login/oauth/authorize?client_id=" + config.github.client_id);
 });
-app.get("/status", function(req,res) {
+app.get("/status", function(req, res) {
   if(req.session.access_token) {
     res.send({authenticated: true});
   } else {
     res.send({authenticated: false});
   }
 });
-app.get("/github-auth", function(req, res) {
+app.get("/authenticated", function(req, res) {
   console.log("code: " + req.query.code);
   request({
     method: 'post',
@@ -49,16 +49,18 @@ app.get("/github-auth", function(req, res) {
       },
       function(error, response, body) {
         var user = JSON.parse(body);
-        authLog.write(new Date().toJSON()+"\t"+user.login+"\n");
+        authLog.write(new Date().toJSON() + "\t" + user.login + "\n");
         req.session.access_token = auth.access_token;
+        req.session.user = user.login;
         res.redirect("http://sdether.github.com/josh.js/githubconsole.html");
       });
   });
 });
-app.get('/hello',function(req,res) {
-	res.send({hello:'world'});
+app.get('/hello', function(req, res) {
+  res.send({hello: 'world'});
 });
-app.get(/^\/(user|users|repos)(\/.*|$)/, function(req,res) {
+app.get(/^\/(user|users|repos)(\/.*|$)/, function(req, res) {
+  authLog.write(new Date().toJSON()+'\t'+req.header('X-Real-IP') + "\t" + req.session.user + "\t" + req.path+ "\n");
   var headers = {
     Accept: 'application/json'
   };
@@ -67,21 +69,26 @@ app.get(/^\/(user|users|repos)(\/.*|$)/, function(req,res) {
   }
   request({
       method: 'get',
-      uri: 'https://api.github.com'+req.path,
+      uri: 'https://api.github.com' + req.path,
       headers: headers
     },
     function(error, response, body) {
-      res.set('Content-Type','application/json; charset=utf-8');
+      res.set('Content-Type', 'application/json; charset=utf-8');
+      res.set('Authenticated', req.session.access_token ? 'true' : 'false')
+      res.set('X-RateLimit-Limit', response.headers['x-ratelimit-limit']);
+      res.set('X-RateLimit-Remaining', response.headers['x-ratelimit-remaining']);
       res.send(body);
     });
 });
-app.get('*', function(req,res) {
+app.get('*', function(req, res) {
+  console.log('redirecting: ' + req.path);
   res.redirect("http://sdether.github.com/josh.js/");
 });
 
-function accessControlHeaders(req,res,next) {
+function accessControlHeaders(req, res, next) {
   res.set('Access-Control-Allow-Credentials', 'true');
   res.set('Access-Control-Allow-Origin', 'http://sdether.github.com');
+  res.set('Access-Control-Expose-Headers', 'X-RateLimit-Limit, X-RateLimit-Remaining, Authenticated');
   next();
 }
 
